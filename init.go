6d44d72a162e2229193d63b42b39909b71b9f0c7
Klyachin Vladimir <klchnv@mail.ru>
2025-10-01 11:30:53 +0300

    Update init.go

--- init.go
+++ init.go
@@ -1,7 +1,12 @@
 package book
-
+import "strconv"
 type Book struct{
 Title string
 Author string
 NumPages int
+}
+
+func (b *Book)ToString()string{
+  txt := strconvFormatInt(b.NumPages,10)
+  return b.Author + " " + b.Title + " (" + txt + ")" 
 }
